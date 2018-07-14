d8dfe624e62792aa260fe9ef03443b5a590cdc34
chenchienwen <reggiechen1982@gmail.com>
2018-07-14 11:31:57 +0800

    Sample code

--- LineWebhook/Controllers/ValuesController.cs
+++ LineWebhook/Controllers/ValuesController.cs
@@ -12,18 +12,13 @@ namespace LineWebhook.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "reggie", "webhook" };
         }
 
         // GET api/values/5
         public string Get(int id)
         {
             return "value";
-        }
-
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
         }
 
         // PUT api/values/5
@@ -34,6 +29,31 @@ namespace LineWebhook.Controllers
         // DELETE api/values/5
         public void Delete(int id)
         {
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post()
+        {
+            string MyLineChannelAccessToken = "QVS7shFmoEt6QiEbel7KFPSgRceEfqTatiFiNAQOqQ+FcColF0xyA+WqZEKs0G50jpJPjh2GaUgtpjeaT0Xr7AXtRxCVo0WpdA9FgLdFl8Uh7FvapYa1GSmMW/7iNq61xjHuGySyGaSNi0MsZLRVxwdB04t89/1O/w1cDnyilFU=";
+
+            try
+            {
+                //取得 http Post RawData(should be JSON)
+                string postData = Request.Content.ReadAsStringAsync().Result;
+                //剖析JSON
+                var ReceivedMessage = isRock.LineBot.Utility.Parsing(postData);
+                //回覆訊息
+                string Message;
+                Message = string.Format("現在時間:{0}  您說了:{1}", DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss"), ReceivedMessage.events[0].message.text);
+                //回覆用戶
+                isRock.LineBot.Utility.ReplyMessage(ReceivedMessage.events[0].replyToken, Message, MyLineChannelAccessToken);
+                //回覆API OK
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return Ok();
+            }
         }
     }
 }
